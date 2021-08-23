cd6b36fdfec1d7eb4166a201c2af7de510cc5cc4
SangwooRyu <mrryu006@naver.com>
2021-08-23 18:38:02 +0900

    fix css and add tab

--- charts/src/App.js
+++ charts/src/App.js
@@ -10,6 +10,13 @@ const { Header, Content, Footer } = Layout;
 function App() {
   const [loading, setLoading] = useState(true);
   const [date, setDate] = useState(null);
+  const [tab, setTab] = useState('weekly');
+
+  function onChangeTab(selected) {
+    setLoading(true);
+    setTab(selected.key);
+    setLoading(false);
+  }
 
   function onChangeDate(date, dateString) {
     setLoading(true);
@@ -29,20 +36,31 @@ function App() {
     <Layout className="layout">
       <Header className = "header">
         <h2 className="titleText"> 메이플스토리 보스 결정 주간 차트 </h2>
-        <Menu theme="light" mode="horizontal" defaultSelectedKeys={['weekly']}>
+        <Menu theme="light" mode="horizontal" defaultSelectedKeys={['weekly']} onClick={onChangeTab}>
           <Menu.Item key='weekly'>주간 차트</Menu.Item>
           <Menu.Item key='flow'>가격 변동</Menu.Item>
         </Menu>
       </Header>
-      <Content className="datePicker">
+      {
+        tab === 'weekly'?
+        <>
+        <Content className="datePicker">
         <div className="dataPickerTitle"> 날짜 선택 </div>
         <DatePicker defaultValue={date} onChange={onChangeDate}/>
-      </Content>
-      <Content style={{ padding: '0px 50px' }}>
-        <div className="site-layout-content">
-          <GraphsPage date={date}/>
-        </div>
-      </Content>
+        </Content>
+        <Content style={{ padding: '0px 50px' }}>
+          <div className="site-layout-content">
+            <GraphsPage date={date}/>
+          </div>
+        </Content>
+        </> :
+        <Content style={{ padding: '24px 50px' }}>
+          <div className="site-layout-content">
+            추가 예정
+          </div> 
+        </Content>
+      }
+
       <Footer style={{ textAlign: 'center' }}> Sangwoo Ryu 2021</Footer>
     </Layout>
   );
