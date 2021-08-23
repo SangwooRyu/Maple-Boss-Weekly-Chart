c74f382d4fdb1be79d2e6b4bc54acea5d067dd71
SangwooRyu <mrryu006@naver.com>
2021-08-23 16:34:27 +0900

    auto-update data if data exists

--- charts/src/page/GraphsPage.js
+++ charts/src/page/GraphsPage.js
@@ -8,8 +8,22 @@ function GraphsPage(){
     const [loading, setLoading] = useState(true);
     const [data, setData] = useState(null);
 
-    async function fetchCsv() {
-        const response = await fetch('/Maple-Boss-Weekly-Chart/data/20210812.csv');
+    function getPreviousMonday(){
+        var date = new Date();
+        var day = date.getDay();
+        var prevThursday = new Date();
+        if(date.getDay() == 0){
+            prevThursday.setDate(date.getDate() - 7);
+        }
+        else{
+            prevThursday.setDate(date.getDate() - (day-1));
+        }
+
+        return prevThursday;
+    }
+
+    async function fetchCsv(filename) {
+        const response = await fetch('/Maple-Boss-Weekly-Chart/data/' + filename);
         // console.log(response);
         const reader = response.body.getReader();
         const result = await reader.read();
@@ -25,7 +39,16 @@ function GraphsPage(){
     useEffect(() => {
         async function loadCsv() {
             setLoading(true);
-            const data = await Papa.parse(await fetchCsv());
+            var prevThursday = new Date();
+            prevThursday.setDate(prevThursday.getDate() - (prevThursday.getDay() + 3) % 7);
+            let year = prevThursday.getFullYear().toString();
+            let month = prevThursday.getMonth() < 10? '0' + (prevThursday.getMonth() + 1).toString() : (prevThursday.getMonth() + 1).toString();
+            let date = prevThursday.getDate() < 10? '0' + prevThursday.getDate().toString() : prevThursday.getDate().toString();
+
+            const targetFileName = year + month + date + '.csv';
+            console.log(targetFileName);
+
+            const data = await Papa.parse(await fetchCsv(targetFileName));
             var refined_data = data.data.reduce(function (pre, value){
                 if (value[0] === "Boss" || value[0] === "")
                     return pre;
